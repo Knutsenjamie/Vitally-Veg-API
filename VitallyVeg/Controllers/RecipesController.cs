bc2fc20a23a2951d9eb6406a97b34a3cdc340d25
“Jamie <knutsenjamie@yahoo.com>
2021-07-16 16:12:37 -0700

    add RecipesController.cs and inject database and jsonfile reference

---
+++ VitallyVeg/Controllers/RecipesController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VitallyVeg.Models;
+
+namespace VitallyVeg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecipesController : ControllerBase
+{
+    private readonly VitallyVegContext _db;
+
+    public 
+
+    public RecipesController(VitallyVegContext db, JsonFileRecipeService recipeService)
+    {
+        RecipeService = recipeService;
+        _db = db;
+    }
+    public JsonFileRecipeService RecipeService { get; }
+
+}
