48ee34da785b133864df290a2da096e4d2b07c03
“Jamie <knutsenjamie@yahoo.com>
2021-07-23 06:36:44 -0700

    change data types for Recipe class in Recipe.cs

--- VitallyVeg/Models/Recipe.cs
+++ VitallyVeg/Models/Recipe.cs
@@ -2,15 +2,13 @@ namespace VitallyVeg.Models
 {
     public class Recipe
     {
-        public int[] RecipeId { get; set; }
-        [JsonPropertyName("img")]
-        public string Image { get; set; }
+        public int RecipeId { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
         public string Cuisine { get; set; }
         public string Ingredients { get; set; }
         public string TotalTime { get; set; }
-        public int[] Yield { get; set; }
+        public int Yield { get; set; }
         public string Instructions { get; set; }
     }
 }
